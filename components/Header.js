815d61593bf6fb740b59e101ca80b5a839dfde75
Tano91 <kyuketsuki91@gmail.com>
2023-11-19 13:39:53 -0500

    modified:   components/Header.js 	modified:   next.config.js 	modified:   package-lock.json 	modified:   package.json 	deleted:    public/favicon.ico 	deleted:    public/next.svg 	new file:   public/rock-staar-EpqBeZlEGk4-unsplash.jpg 	deleted:    public/vercel.svg 	new file:   public/yvFull_G.png 	new file:   public/yvFull_G_B.png 	new file:   public/yvFull_W.png 	new file:   public/yvIcon_G.png 	new file:   public/yvIcon_G_B.png 	new file:   public/yvIcon_W.png

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,31 @@
+import Image from "next/image"
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import yvFull_G from '@/public/yvFull_G.png'
+
 function Header() {
   return (
-    <div>
-      <h1>I am Header</h1>
+    <header className="sticky to-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
 
-      {/* Left */}
-      <div>
-
+      {/* Left - Logo */}
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src={yvFull_G}
+          fill
+          alt="YaadVentures Logo"
+          style={{objectFit: "contain", objectPosition:"left"}}	
+        />
       </div>
 
-      {/* Middle */}
-
-      <div>
-        
-      </div>
+      {/* Middle - Search */}
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input type="text" placeholder="Search YaadVentures"className="flex-grow pl-5 bg-transparent outline-none"/>
+        <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-emerald-300 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
+      </div> 
 
       {/* Right */}
 
-      <div>
-
-      </div>
-    </div>
+      <div></div>
+    </header>
 
     
   )
@@ -27,4 +33,4 @@ function Header() {
 
 export default Header
 
-// 23:00
+// 49:07
