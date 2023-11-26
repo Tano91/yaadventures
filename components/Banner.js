f7fa04d46c072ad39b2d613ee9e10129906d2d45
Tano91 <kyuketsuki91@gmail.com>
2023-11-26 14:39:54 -0500

    modified:   components/Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -12,7 +12,7 @@ const Banner = () => {
         className='brightness-50 contrast-125'
         />
         <div className='absolute top-1/2 w-full text-center'>
-            <p className='text-sm sm:text-lg text-white'>
+            <p className='text-lg sm:text-xl text-white'>
               Let's find your next YaadVenture!</p>
 
             <button className='text-white bg-emerald-600 px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl hover:bg-emerald-800 hover:scale-105 active:scale-90 transition duration-150'>Let's Go!</button>
